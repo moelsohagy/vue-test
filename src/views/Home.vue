1ed5adb285dbe7920bca56e535684398c2b0fd65
Mohamed ElSohagy <mohamedelsohagy9@gmail.com>
2021-01-10 02:10:44 +0200

    check git

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your My Vue.js App" />
+    <HelloWorld msg="Welcome to Your My fav Vue.js App" />
   </div>
 </template>
 
